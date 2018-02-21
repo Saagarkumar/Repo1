554fc675f92436bb21e4ab4ba4dfa683b9bd9dfe
root <root@localhost.localdomain>
2018-02-20 20:46:11 -0500

    this works now

--- fruit_basket.py
+++ fruit_basket.py
@@ -3,12 +3,19 @@
 
 fruit_basket = ["orange", "apple", "watermelon", "grape", "cherry"]
 guess = raw_input("Guess what fruit is in the basket.\n")
-if guess in fruit_basket:
-	print "correct"
+i = 0
+for i in range(0,6,1):
 
-else :
-    	print "wrong"
+	if guess in fruit_basket:
+		print "correct"
 
+	elif i <= 5:
+		print "guess again\n"
+
+
+	else :
+    		print "wrong"
+	
 
 
 
