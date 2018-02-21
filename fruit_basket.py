de7333a5ccf17d432ed168aa33a4b9fed3f038f7
root <root@localhost.localdomain>
2018-02-20 20:40:30 -0500

    this works now

---
+++ fruit_basket.py
@@ -0,0 +1,14 @@
+
+
+
+fruit_basket = ["orange", "apple", "watermelon", "grape", "cherry"]
+guess = raw_input("Guess what fruit is in the basket.\n")
+if guess in fruit_basket:
+	print "correct"
+
+else :
+    	print "wrong"
+
+
+
+
